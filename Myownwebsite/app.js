34be3ab004db96979d99b840232e21a8c24d6f14
TecJoJo <107677643+TecJoJo@users.noreply.github.com>
2022-07-18 00:16:21 +0300

    Add files via upload

---
+++ Myownwebsite/app.js
@@ -0,0 +1,8 @@
+const hamburgerToggle = document.querySelector(".hamburger")
+const HamburgerMenuSlider = document.querySelector(".menu-content")
+
+function ToggleHamburgerMenu(){
+    console.log("clicked")
+    HamburgerMenuSlider.classList.toggle("isactive")
+}
+hamburgerToggle.addEventListener("click",ToggleHamburgerMenu)
